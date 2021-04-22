9723ed752de582d7ef5770bf0eccbee8247be16f
Damian Sloane <damian@capacitorlabs.com.au>
2021-04-22 16:47:23 +1000

    Work in progress

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:14
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update; \
+    apt-get install -y --no-install-recommends jq; \
+    rm -rf /var/lib/apt/lists/*;
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -eo pipefail
+cd "${GITHUB_WORKSPACE}/${INPUT_PATH}" || { echo "Could not find specified path"; exit 1; }
+if [[ ! -z "${INPUT_NPM_TOKEN}" ]]; then
+  echo "Exporting NPM token"
+  export NPM_TOKEN="${INPUT_NPM_TOKEN}"
+fi
+yarn build
